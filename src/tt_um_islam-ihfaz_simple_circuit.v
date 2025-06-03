c5e02f55cd533e7faf04c894d9e5def6ea3f545d
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-06-03 14:56:22 -0400

    Update tt_um_islam-ihfaz_simple_circuit.v

--- src/tt_um_islam-ihfaz_simple_circuit.v
+++ src/tt_um_islam-ihfaz_simple_circuit.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_islam_ihfaz_simple_circuit (
+module tt_um_islam-ihfaz_simple_circuit (
     input  wire [7:0] ui_in,     // Dedicated inputs
     output wire [7:0] uo_out,    // Dedicated outputs
     input  wire [7:0] uio_in,    // IOs: Input path
